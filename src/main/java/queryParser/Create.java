7784633fe72395dc77c36d6c1b61e3fcb02ac8a0
Manjinder <mn758600@dal.ca>
2021-08-03 02:42:30 +0530

    Validator for CREATE operation

--- src/main/java/queryParser/Create.java
+++ src/main/java/queryParser/Create.java
@@ -1,4 +1,15 @@
 package queryParser;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+
 public class Create {
-}
+
+    public static void createParse(Matcher createTable, String username) throws IOException {
+        }
+    }
