20f4d17db987642aeb1033edad56982c20aaf5db
Manjinder <mn758600@dal.ca>
2021-08-03 05:17:00 +0530

    Validator for INSERT Query

--- src/main/java/queryValidator/InsertValidator.java
+++ src/main/java/queryValidator/InsertValidator.java
@@ -1,28 +1,24 @@
 package queryValidator;
-import queryParser.Create;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import dataLogs.DataLogs;
+import queryParser.Create;
 
-public class CreateValidator {
+public class InsertValidator {
     static DataLogs log = new DataLogs();
 
-    public static void main(String[] args) throws IOException {
-        String username="Manjinder";
-        validateCreate(username);
-    }
-    private static final Pattern CREATE_REGEX =
-            Pattern.compile("(?i)(CREATE\\sTABLE\\s(\\w+)\\s?\\(((?:\\s?\\w+\\s\\w+\\(?[0-9]*\\)?,?)+)\\)\\s?;)");
+    private static final Pattern INSERT_REGEX =
+            Pattern.compile("(?i)(INSERT\\sINTO\\s+(\\w+)\\s+\\(([\\s\\S]+)\\)\\s+VALUES\\s+\\(([\\s\\S]+)\\);)");
 
     public static void validateCreate(String username) throws IOException {
         System.out.println("Enter your SQL Query");
         Scanner scanner = new Scanner(System.in);
         String query;
         while (scanner.hasNext() && !((query = scanner.nextLine()).equalsIgnoreCase("exit"))) {
-            Matcher createTableSQL = CREATE_REGEX.matcher(query);
+            Matcher createTableSQL = INSERT_REGEX.matcher(query);
 
             if (createTableSQL.find())
                 Create.createParse(createTableSQL, username);
@@ -30,5 +26,5 @@ public class CreateValidator {
                 log.logger(Level.WARNING, "INVALID SQL Query !!");
         }
     }
-}
 
+}
