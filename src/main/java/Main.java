c57ccc1c938c3f88f3ac6e1068f4e66091b86295
Jagadeeswara Aditya Busam <jg469380@dal.ca>
2021-08-04 11:58:26 -0300

    Adding ERD

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
+import ERDGenerator.ERD;
 import SQLDump.DataDump;
 import dataLogs.DataLogs;
+import queryValidator.CreateValidator;
 import userAuthentication.UserAuthentication;
 
+import javax.swing.text.LabelView;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,45 +16,57 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        DataLogs log = new DataLogs();
         System.out.println("***** Welcome to Database Management System *****");
         System.out.println("MAIN MENU:");
         System.out.println("1. Login");
         System.out.println("2. Register");
-        System.out.println("3. Perform DB Operations");
-        System.out.println("4. Generate ERD");
-        System.out.println("5. General SQL Dump");
-        System.out.println("6. Exit");
         int user = sc.nextInt();
+        String username = "";
+        String password = "";
 
-        String username = null;
-        String password = null;
-        if(user == 1 || user == 2) {
+        if (user == 1 || user == 2) {
             System.out.println("Username: ");
             username = sc.next();
             System.out.println("Password: ");
             password = sc.next();
-        }
 
-        if (user == 1) {
-            UserAuthentication.login(username, password);
-        } else if (user == 2) {
-            UserAuthentication.registration(username, password);
-            UserAuthentication.login(username, password);
-        }
-
-        if(user > 6) {
-            System.out.println("Please select a valid option");
-        }
-
-        if(user == 5) {
-            if(list.size() !=0) {
-                DataDump.sqlDump(list);
+            if(user == 2) {
+                UserAuthentication.registration(username, password);
             }
-            else {
+            UserAuthentication.login(username, password);
+            performOperations(username);
+        }
+        else if (user > 3) {
+            System.out.println("Select correct option");
+        }
+    }
+
+    private static void performOperations(String username) {
+        DataLogs log = new DataLogs();
+        System.out.println("Please select the following operation");
+        System.out.println("1. Perform DB Operations");
+        System.out.println("2. Generate ERD");
+        System.out.println("3. Exit");
+        Scanner scanner = new Scanner(System.in);
+        int operation = scanner.nextInt();
+        if(operation == 1) {
+            //example
+            try {
+                CreateValidator.validateCreate(username,list);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            if (list.size() != 0) {
+                log.logger(Level.INFO,"Generating SQL Dump");
+                DataDump.sqlDump(list);
+            } else {
                 log.logger(Level.WARNING, "No commands have been executed");
             }
         }
-
+        else if (operation == 2) {
+            ERD.generateERD();
+        }
     }
 }
